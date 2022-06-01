4f4b8f330dcf76472dc3337d7339c79232d620dc
engtasneemmaq <tasneemmaq@yahoo.com>
2022-06-01 15:16:28 +0300

    validator done

---
+++ __tests__/validator.test.js
@@ -0,0 +1,20 @@
+'use strict';
+const validator = require('../src/middleware/validator');
+const supertest = require('supertest');
+const server = require('../src/server');
+const request = supertest(server.app);
+
+describe('validator Test', () => {
+  it('Should check if the name is string and not empty', async () => {
+    const response = await request.get('/person?name=tasneem');
+    expect(response.status).toBe(200);
+  });
+  it('Should check if the name is not string', async () => {
+    const response = await request.get(`/person?name=678`);
+    expect(response.status).toBe(500);
+  });
+  it('Should check if the name is not string and not empty', async () => {
+    const response = await request.get('/person?name=');
+    expect(response.status).toBe(500);
+  });
+});
---
+++ src/middleware/validator.js
@@ -0,0 +1,18 @@
+'use strict';
+function validator() {
+  return (req, res, next) => {
+    let regx = /^[a-zA-Z]+$/;
+    if (regx.test(req.query.name)) {
+      next();
+    }
+    else if (req.query.name === "") {
+      req.query.name = "";
+      next('The name is required');
+    }
+    else if (!regx.test(req.query.name)) {
+      req.query.name = "wrong Name";
+      next('The name should be a string');
+    }
+  }
+}
+module.exports = validator;
