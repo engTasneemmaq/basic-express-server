e712a1437403c4607acdf6e896e0dceb6eb5ee09
engtasneemmaq <tasneemmaq@yahoo.com>
2022-06-02 18:28:06 +0300

    error 500

--- src/error-handlers/500.js
+++ src/error-handlers/500.js
@@ -1,9 +1,16 @@
 'use strict';
-
 module.exports = (error, req, res, next) => {
+  if (req.query.name === '') {
     res.status(500).send({
-        code: 500,
-        route: req.path,
-        message: `Server Error:${error.message}`,
-    });
+      code: 500,
+      route: req.path,
+      message: `Server Error:${error}`,
+    })
+  } else if (req.query.name === 'wrong Name') {
+    res.status(500).send({
+      code: 500,
+      route: req.path,
+      message: `Server Error:${error}`,
+    })
+  };
 };
